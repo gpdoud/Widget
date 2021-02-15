9e5477f890829588a9a246357bb62aab22109471
Greg Doud <gpdoud@gmail.com>
2021-02-15 14:34:13 -0500

    Add project files.

---
+++ Widget/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Widget {
+    class Program {
+        static void Main(string[] args) {
+
+            var w1 = new Widget("LARGE", "RED");
+            Console.WriteLine($"A {w1.Size}, {w1.Color} widget costs {w1.Price()}");
+            var wsw = new Widget("SMALL", "WHITE");
+            Console.WriteLine($"A {wsw.Size}, {wsw.Color} widget costs {wsw.Price()}");
+        }
+    }
+}
